5416db719b0eb1650360d5b05051fa280812f853
Brian Bugle <bbugle@sphereoi.com>
2017-06-07 19:31:04 -0400

    Adds ability to select yours and enemy character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,24 +19,48 @@ $(document).ready(function() {
 				hp: 100
 			}
 		},
+		yourCharacter: "",
 		displayCharacter: function(args) {
+			const character = game.characters[args.name];
 			$(args.where).append(`<span class="character ${args.class}" id="${args.name}">${args.name}
-				<img src="assets/images/${args.character.image}" alt="${args.name}" class="character-image">
-				${args.character.hp}</span>`);
+				<img src="assets/images/${character.image}" alt="${args.name}" class="character-image">
+				${character.hp}</span>`);
 		}
 	}
 
 	for (var character in game.characters) {
 		game.displayCharacter({
 			name: character,
-			character: game.characters[character],
-			where: "#characters",
-			class: "friendly"
+			where: '#characters',
+			class: 'friendly'
 		});
 	}
 
-	$('.character').click(function(e) {
-		console.log(e.currentTarget.id);
-	});
+	$('div#characters').on('click', 'span.character', function(e) {
+		$('#characters').empty();
 
+		const where = !game.yourCharacter ? '#your-character' : '#enemy-character';
+		const classes = !game.yourCharacter ? 'friendly' : 'enemy';
+		console.log(where + ' ' + classes);
+
+		game.displayCharacter({
+			name: e.currentTarget.id,
+			where: where,
+			class: classes
+		});
+
+		if (!game.yourCharacter) {
+			for (var character in game.characters) {
+				if (character !== e.currentTarget.id) {
+					game.displayCharacter({
+						name: character,
+						where: '#characters',
+						class: 'enemy'
+					});
+				}
+			}
+		}
+
+		game.yourCharacter = e.currentTarget.id;
+	});
 });
